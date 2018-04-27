8c44e4c9c1c2a4ade0e723073adac71642b9576e
Hegedüs Dániel <dani940414@gmail.com>
2018-04-27 16:29:53 +0200

    basic structure, and opengl

---
+++ VectorScope2_0/oglwidget.cpp
@@ -0,0 +1,97 @@
+#include "oglwidget.h"
+#include <QOpenGLShaderProgram>
+#include <QDebug>
+
+OGLWidget::OGLWidget(QWidget *parent) : QOpenGLWidget(parent)
+{
+    h = this->parentWidget()->size().height();
+    w = this->parentWidget()->size().height();
+}
+
+OGLWidget::~OGLWidget() {
+    vao.destroy();
+    vbo[0].destroy();
+    vbo[1].destroy();
+    delete program;
+}
+
+void OGLWidget::initializeGL()
+{
+    vertices << QVector3D(0.00f, 0.75f, 1.0f) << QVector3D( 0.75f, -0.75f, 1.0f) << QVector3D(-0.75f, -0.75f, 1.0f);
+
+    colors << QVector3D(1.0f, 0.0f, 0.0f) << QVector3D(0.0f, 1.0f, 0.0f) << QVector3D(0.0f, 0.0f, 1.0f);
+
+    initializeOpenGLFunctions();
+    glClearColor(0,0,0,1);
+
+    connect(this, SIGNAL(frameSwapped()), this, SLOT(update()));
+
+    program = new QOpenGLShaderProgram();
+    program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    program->link();
+    program->bind();
+
+    u_matrix = program->uniformLocation("matrix");
+    matrix.setToIdentity();
+
+    vbo[0].create();
+    vbo[0].bind();
+    vbo[0].setUsagePattern(QOpenGLBuffer::DynamicDraw);
+    vbo[0].allocate(vertices.constData(), vertices.size() * sizeof(QVector3D));
+    vbo[0].release();
+
+    vbo[1].create();
+    vbo[1].bind();
+    vbo[1].setUsagePattern(QOpenGLBuffer::DynamicDraw);
+    vbo[1].allocate(colors.constData(), colors.size() * sizeof(QVector3D));
+    vbo[1].release();
+
+    vao.create();
+    vao.bind();
+
+    vbo[0].bind();
+    program->enableAttributeArray(0);
+    program->setAttributeBuffer(0, GL_FLOAT, 0, 3, 0);
+    vbo[0].release();
+    vbo[1].bind();
+    program->enableAttributeArray(1);
+    program->setAttributeBuffer(1, GL_FLOAT, 0, 3, 0);
+    vbo[0].release();
+
+    vao.release();
+    program->release();
+
+}
+
+void OGLWidget::resizeGL(int w, int h)
+{
+    this->h = h;
+    this->w = w;
+}
+
+void OGLWidget::paintGL()
+{
+    if(w <= h) {
+        h = w;
+    } else {
+        w = h;
+    }
+    glViewport(0, 0,w, h);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    makeCurrent();
+
+    qDebug() << matrix;
+
+    program->bind();
+    vao.bind();
+    program->setUniformValue(u_matrix, matrix);
+    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+    vao.release();
+    program->release();
+}
+
+void OGLWidget::update()
+{
+    QOpenGLWidget::update();
+}
---
+++ VectorScope2_0/oglwidget.h
@@ -0,0 +1,68 @@
+#ifndef OGLWIDGET_H
+#define OGLWIDGET_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLShader>
+#include <QMatrix4x4>
+#include <GL/GLU.h>
+#include <GL/GL.h>
+
+class OGLWidget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+public:
+    OGLWidget(QWidget *parent);
+    ~OGLWidget();
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
+protected slots:
+    void update();
+
+private:
+    QOpenGLBuffer vbo[2];
+    QOpenGLVertexArrayObject vao;
+    QOpenGLShaderProgram *program;
+    QVector<QVector3D> vertices;
+    QVector<QVector3D> colors;
+    QMatrix4x4 matrix;
+    int u_matrix;
+    int w;
+    int h;
+};
+
+static const char *vertexShaderSource = R"(
+    #version 330
+    layout(location = 0) in vec3 position;
+    layout(location = 1) in vec3 color;
+    out vec4 vColor;
+
+    uniform mat4 matrix;
+
+    void main(void)
+    {
+        gl_Position = vec4(position, 1) * matrix;
+        vColor = vec4(color, 1);
+    }
+)";
+
+static const char *fragmentShaderSource = R"(
+      #version 330
+      in vec4 vColor;
+      out vec4 fColor;
+
+      void main(void)
+      {
+          gl_FragColor = vColor;
+      }
+
+)";
+
+#endif // OGLWIDGET_H
